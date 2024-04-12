d35129065ca301b9e484879fa9a82874ff3a738e
Mike K <michaelkucukarslan@gmail.com>
2024-04-11 22:29:27 -0700

    Played with exceptions in python

--- exceptions_handling.py
+++ exceptions_handling.py
@@ -4,3 +4,7 @@ while True:
     try:
         index = int(input("Which index shall we look for? "))
         print("That was element", sequence[index])
+    except IndexError:
+        print("Not in range.")
+    except ValueError:
+        print("Not an integer.")
