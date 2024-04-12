76edc255880b8daf6025174c6ab350df99e6e442
Mike K <michaelkucukarslan@gmail.com>
2024-04-12 10:34:52 -0700

    Finished constant design pattern practice

--- constant_design_pattern.py
+++ constant_design_pattern.py
@@ -1,18 +1,28 @@
+# Music Volumes
+GENTLE_MUSIC_VOLUME = 2
+MEDIUM_MUSIC_VOLUME = 6
+LOUD_MUSIC_VOLUME = 11
+MUSIC_VOLUME = GENTLE_MUSIC_VOLUME
+
+WATER_PER_GUEST_IN_ML = 400
+SPOONS_PER_GUEST = 2
+WAIT_TIME_TO_TAKE_ORDER = 2
 
 def serve_group_of_two():
     """
    Serves a party of two hungry customers.
    Ensures an unforgettable experience!
    """
+    number_of_guests = 2
     greet_guests()
-    table_number = prepare_table(2)
-    for guest in range(0, 2):
-        lay_spoons_on_table(2)
+    table_number = prepare_table(number_of_guests)
+    for guest in range(0, number_of_guests):
+        lay_spoons_on_table(SPOONS_PER_GUEST)
     lead_customers_to_table(table_number)
-    fill_water_carafe(800)
-    adjust_music_volume(2)
-    present_menus(2)
-    wait_minutes(2)
+    fill_water_carafe(number_of_guests * WATER_PER_GUEST_IN_ML)
+    adjust_music_volume(MUSIC_VOLUME)
+    present_menus(number_of_guests)
+    wait_minutes(WAIT_TIME_TO_TAKE_ORDER)
     drinks_order = take_order(table_number)
     process_order(drinks_order)
     # ...
